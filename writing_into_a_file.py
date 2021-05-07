c3575dcf68ca12b9dcbfe73ec02161f2480eb2b1
Oliwier Szymański <oliwier.szymanski112@gmail.com>
2021-05-07 10:39:13 +0200

    cleaning the code

--- writing_into_a_file.py
+++ writing_into_a_file.py
@@ -8,12 +8,17 @@ file_name = "moj_pierwszy_plik.txt"
 f = open( file_name, 'wb' ) 		# open the file;
 										# note : if the file does not exist it will be created automatically;
 
+	
+	# TODO:	add file to this repo path: file_name.txt
+
+
 # to write into the txt file, use the .write function
 
-f.write("Zapis informacji do pliku \n\n\n")
-	# "\n" indicates a new line
+f.write("Zapis informacji do pliku \n\n\n")		# "\n" indicates a new line
 
-f.write("po kazdej wyprowadzonym stringu trzeba przejsc do nowej linii \n")
+
+
+f.write("po kazdej wyprowadzonym stringu trzeba przejsc do nowej linii \n")		# editing the file
 f.write("wypisujac znak specjalny \\n albo dodajac \\na koncu naszego stringu \n")
 
 f.write("\n\n")
@@ -34,7 +39,7 @@ notes :
 f.close()			# close the file; this should always be the last line of code after writing into a file
 						# if you write any code related to the file after this, it will not be recognised
 
-print "\n\n"
-print "Pisanie do pliku zakonczone"
+print("\n\n")
+print("Pisanie do pliku zakonczone")
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
